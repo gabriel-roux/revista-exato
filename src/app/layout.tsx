7ce3f5de848fdc99eaf92decf4f80d31e5306ef7
Gabriel Fonseca <fonsecagabriel9781@gmail.com>
2024-01-29 15:16:42 -0300

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.png" />
         <link href="/dist/output.css" rel="stylesheet" />
       </head>
       <body className={`${manrope.variable} ${ptserif.variable}`}>
